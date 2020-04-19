b9122b5b85e8611f235febbb15f02940e0d4ef35
Vardhan NS <vardhan@edureka.co>
2020-04-19 19:49:30 +0530

    Update body.php

--- php/online-shopping-system/body.php
+++ php/online-shopping-system/body.php
@@ -405,7 +405,7 @@
 									</div>
 									<div class="product-body">
 										<p class="product-category">Category</p>
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
+										<h3 class="product-name"><a href="#">SAMSUNG GALAXY S7 EDGE</a></h3>
 										<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
 									</div>
 								</div>
