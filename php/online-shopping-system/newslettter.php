ae41f1d25507dcbef1d957fb657323486f60d697
Vardhan NS <vardhan@edureka.co>
2020-04-19 19:51:11 +0530

    Update newslettter.php

--- php/online-shopping-system/newslettter.php
+++ php/online-shopping-system/newslettter.php
@@ -10,7 +10,8 @@
 					<div class="col-md-12">
 					
 						<div class="newsletter">
-							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<p>Sign up to Edureka's Newsletter 
+for <strong>OFFER UPDATES</strong></p>
 							<form id="offer_form" onsubmit="return false">
 								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
 								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
